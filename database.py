a1a193151c10891f9777e51069c5a4d2ca3a5cfa
rohitrkvarathe111 <rohitrkvarathe111@fmail.com>
2025-07-25 15:47:18 +0530

    commit db url

--- database.py
+++ database.py
@@ -3,7 +3,12 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5432/nakspay" 
+# DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5432/nakspay" 
+DATABASE_URL = (
+    "postgresql+asyncpg://nakspay:IkEvKrMmYjx9JoxOTKKciLUoJTKRtuGV"
+    "@dpg-d21lfd63jp1c7380pes0-a.oregon-postgres.render.com:5432/nakspay"
+)
+
 
 engine = create_async_engine(DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
